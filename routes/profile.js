f5c8e9cb9334fe0702bc98630f1635552222ca4c
skaiser33 <skaiser33@gmail.com>
2021-04-06 17:58:46 -0400

    adds functionality for profile setup form to update user instance

--- routes/profile.js
+++ routes/profile.js
@@ -10,18 +10,32 @@ router.get('/new/:id', (req, res) => {
     .then((genres) => {
       db.city.findAll()
       .then((cities) => {
-      res.render('profile/new', {user: req.user, instruments: instruments, genres: genres, cities: cities})
+        db.user.findOne({    
+          where: {id: req.user.id},         
+        }).then((user) => {
+        res.render('profile/new', {user: req.user, instruments: instruments, genres: genres, cities: cities})
+        })
+      })
     })
-    })
-    })
-    })
+  })
+})
 
 //post new profile form
 router.post('/new/:id', (req, res) => {
-
   db.user.update({
+    isBand: false, //TODO value from radio form?
+    name: req.body.name,       
+    influences: [req.body.influences],
+    recordingCredits: [req.body.recordingCredits],
+    canRecordRemotely: false, //TODO value from radio form?
+    spotifyEmbedUrl: req.body.spotifyEmbedUrl,
+    soundcloudEmbedUrl: req.body.soundcloudEmbedUrl,
+    youtubeEmbedUrl: req.body.youtubeEmbedUrl,
+    localDraw: req.body.localDraw,
+    nationalDraw: req.body.nationalDraw,
+    //TODO: associate city, instruments, genres
   }, {
-    where: { id: req.body.id }
+    where: { id: req.params.id }
     // [note: the ‘user’ argument does not return the user data, just the number of rows updated]
   }).then(function(user) {
     console.log("********", user, "********");
@@ -29,12 +43,12 @@ router.post('/new/:id', (req, res) => {
   }).catch(error => {
     // FLASH
     req.flash('error', error.message);
-    res.redirect(`/profile/new/${user.id}`);
+    res.redirect(`/profile/new/${req.params.id}`);
+    // res.redirect(`/profile/new/${user.id}`);
   });
 });
 
-
-
+//get new profile form
 router.get('/:id', (req, res) => {
   db.user.findOne({    
     where: {id: req.user.id},
