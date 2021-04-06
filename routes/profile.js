5b48d89763a4155fc439e780fcaf942a0d053df1
skaiser33 <skaiser33@gmail.com>
2021-04-05 21:00:01 -0400

    adds user id param to profile route

--- routes/profile.js
+++ routes/profile.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-
-
 router.get('/:id', (req, res) => {
-  res.render('profile');
-});
-
+  console.log("****************",req.user.id);
+  db.user.findOne({
+    
+    where: {id: req.user.id},
+    include: [db.city]
+  }).then((user) => {res.render('profile', {user: user})
+  })
+})
 
 module.exports = router;
