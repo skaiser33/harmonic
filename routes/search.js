4125c1106c8b68ec2ae139201712d13195eae8ff
skaiser33 <skaiser33@gmail.com>
2021-04-07 15:32:05 -0400

    adds M:M filtering to user search

--- routes/search.js
+++ routes/search.js
@@ -6,7 +6,22 @@ const db = require('../models');
 //get index of search results
 router.get('/', (req, res) => {
   db.user.findAll({    
-    // where: {id: req.params.id},
+    // include: [db.city, db.instrument, db.collaboration, db.genre],
+    include: [
+      { model: db.instrument,
+        where: { name: "Cello" },
+      },
+    ],  
+    where: {
+      // '$user.instrument.name$': "Drums"
+      isBand: false,
+      // cityId: 1
+      //instrument M:M
+      //collaboration M:M
+      //influences (%___%):
+      //genres:
+    },
+    order: [['name', 'ASC']]
     // include: [db.city, db.instrument, db.genre, db.collaboration]
   }).then((foundUsers) => {
     // console.log(user.collaborations);
