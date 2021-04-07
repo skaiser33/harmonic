7d06e98418348b42de2aac58b0679e7693ea7806
skaiser33 <skaiser33@gmail.com>
2021-04-07 17:07:29 -0400

    builds search form

--- routes/search.js
+++ routes/search.js
@@ -2,24 +2,46 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+router.get('/', (req, res) => {
+  db.instrument.findAll()
+  .then((instruments) => {
+    db.genre.findAll()
+    .then((genres) => {
+      db.collaboration.findAll()
+      .then((collaborations) => {
+        db.city.findAll()
+        .then((cities) => {
+          // DO I NEED THE USER?
+          // db.user.findOne({    
+          //   where: {id: currentUser.id},         
+          // }).then((user) => {
+          res.render('search/main', {user: req.user, instruments: instruments, genres: genres, cities: cities, collaborations: collaborations})
+          // })
+        })
+      })
+    })
+  })
+})
 
 //get index of search results
-router.get('/', (req, res) => {
+router.get('/index', (req, res) => {
   db.user.findAll({    
     // include: [db.city, db.instrument, db.collaboration, db.genre],
     include: [
       { model: db.instrument,
-        where: { name: "Cello" },
+        where: { name: "Drums" },
+      },
+      { model: db.collaboration,
+        where: { type: "Shows" },
+      },
+      { model: db.genre,
+        where: { name: "Experimental" },
       },
     ],  
     where: {
-      // '$user.instrument.name$': "Drums"
       isBand: false,
-      // cityId: 1
-      //instrument M:M
-      //collaboration M:M
+      cityId: 1
       //influences (%___%):
-      //genres:
     },
     order: [['name', 'ASC']]
     // include: [db.city, db.instrument, db.genre, db.collaboration]
